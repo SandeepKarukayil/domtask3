e30d067a4f7f8fb8200bf8c6c4e984d0fa7ad66d
Sandeep Karukayil <115607146+SandeepKarukayil@users.noreply.github.com>
2023-02-20 13:28:47 +0530

    Add files via upload

---
+++ task3rdpart.js
@@ -0,0 +1,10 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById("main-header")
+
+header.style.borderBottom = 'solid 3px #000';
+
+var subtitle = document.getElementById('subtitle');
+var add = document.getElementById("add");
+
+add.style.fontWeight = "bold";
+add.style.color = "green";
